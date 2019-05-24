cdaf7e72850c1485bc942640fab74311d34d693d
Miikis <28997062+Miikis@users.noreply.github.com>
2019-05-23 19:45:50 -0700

    Add scrolling to testimonials

--- src/scripts/_testimonial.js
+++ src/scripts/_testimonial.js
@@ -1,5 +1,64 @@
-import {} from "./_utils"
+import { $qs } from "./_utils"
+
+class Carousel {
+    constructor() {
+        this.$ = $qs(".carousel")
+        this.$viewport = this.$.$qs(".viewport")
+        this.$slide = this.$viewport.$qs(".slide")
+
+        this.currentPosition = 0
+        this.contentLength = this.$slide.$qsa(".content").length
+
+        this.$viewport.addEventListener(
+            "click",
+            this.handleNavigationEvent.bind(this)
+        )
+    }
+
+    updateCurrentPosition(newPosition) {
+        // If the new position is < 0
+        if (newPosition < 0) {
+            this.currentPosition = this.contentLength - 1
+            return
+        }
+
+        // If the new position is >= contentLength
+        if (newPosition >= this.contentLength) {
+            this.currentPosition = 0
+            return
+        }
+
+        this.currentPosition = newPosition
+    }
+
+    getFormattedPosition() {
+        return this.currentPosition === 0
+            ? `0`
+            : `-${this.currentPosition * 100}%`
+    }
+
+    handleNavigationEvent({ target, clientX }) {
+        const { left, width } = target.getBoundingClientRect()
+        const x = clientX - left
+
+        if (x < width / 2) {
+            return this.next()
+        }
+
+        this.previous()
+    }
+
+    next() {
+        this.updateCurrentPosition(this.currentPosition + 1)
+        this.$slide.style.left = this.getFormattedPosition()
+    }
+
+    previous() {
+        this.updateCurrentPosition(this.currentPosition - 1)
+        this.$slide.style.left = this.getFormattedPosition()
+    }
+}
 
 export default () => {
-    console.log("sdfsdf")
+    new Carousel()
 }
