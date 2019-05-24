0229ff5e11edffb9f70bd6ce95b6e75f8f880a65
Miikis <28997062+Miikis@users.noreply.github.com>
2019-05-23 23:10:36 -0700

    Separate home page js components from about page

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -1,7 +1,3 @@
 import setupMobileNavigation from "./_mobile_nav"
-import startErasingType from "./erasing-type"
-import setupCarousel from "./_testimonial"
 
-startErasingType()
 setupMobileNavigation()
-setupCarousel()
